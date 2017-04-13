ba57292e701b2ef2d1924626a79f36b772972792
John Murphy <dearjohnm@gmail.com>
2017-04-13 13:17:31 -0700

    made it so rollup.config.js isn't ignored by git and created new rollup config

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+export default {
+  entry: 'dist/index.js',
+  dest: 'dist/bundles/ng2-edu.umd.js',
+  sourceMap: false,
+  format: 'umd',
+  moduleName: 'ng.edu',
+  globals: {
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common',
+    '@angular/forms': 'ng.forms',
+    '@angular/router': 'ng.router'
+  }
+}
