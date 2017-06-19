f76ff1204ebdd234832518dceb95a5a7abae3458
John Murphy <murphy11@ad.unlv.edu>
2017-06-19 15:37:11 -0700

    added flashcards module

---
+++ components/flashcards/flashcards.ts
@@ -0,0 +1,246 @@
+import { NgModule, Component, OnInit, Input, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'oe-flashcards',
+  template: `
+    <div class="cardset-wrapper clearfix">
+      <!-- Card -->
+      <div class="cardset" [ngClass]="{'flip': card.flipped, 'show': showCard(i)}" *ngFor="let card of data.cards; let i = index;">
+        <!-- Front -->
+        <div class="card front" (click)="flipCard(i)">
+          <div class="card-contents">
+            <img *ngIf="card.front.image" [src]="card.front.image" [alt]="card.front.alt">
+            <p *ngIf="card.front.copy">
+              <strong>{{card.front.copy}}</strong>
+            <p>
+          </div>
+        </div>
+        <!-- Back -->
+        <div class="card back" (click)="flipCard(i)">
+          <div class="card-contents">
+            <img *ngIf="card.back.image" [src]="card.back.image" [alt]="card.back.alt">
+            <p *ngIf="card.back.copy">
+              <strong>{{card.back.copy}}</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Next/Prev Controls -->
+    <div class="flashcard-controls" *ngIf="!data.graded">
+      <p>
+        {{data.cardNumber + 1}} of {{data.cards.length}}
+      </p>
+      <button value="previous card" aria-label="previous card" class="pure-button pure-button-primary button-oe" (click)="changeCard('prev')" [disabled]="firstCard()">
+        <span class="fa fa-step-backward"></span>
+      </button>
+      <button value="next card" aria-label="next card" class="pure-button pure-button-primary button-oe" (click)="changeCard('next')" [disabled]="lastCard()">
+        <span class="fa fa-step-forward"></span>
+      </button>
+    </div>
+  `,
+  styles: [`
+    @media screen and (min-width: 64em) {
+        .pure-g.interactive {
+            background: #efefef;
+            padding: 1.5rem 0;
+        }
+    }
+
+    div.cardset-wrapper {
+        position: relative;
+        margin: 1.5em auto;
+        max-width: 42rem;
+    }
+
+        div.flashcard-controls {
+            text-align: center;
+            margin-bottom: 1.5rem;
+        }
+
+        div.cardset {
+            width: 100%;
+            height: 24rem;
+            float: left;
+            display: none;
+            position: relative;
+            box-sizing: border-box;
+            -webkit-perspective: 960px;
+            perspective: 960px;
+            -webkit-transition: all .5s ease;
+            transition: all .5s ease;
+            -webkit-tap-highlight-color: transparent;
+        }
+
+        div.cardset.show {
+          display: block;
+        }
+
+        div.cardset:hover {
+            cursor: pointer;
+            -webkit-transition: all .5s ease;
+            transition: all .5s ease;
+        }
+
+            div.cardset div.card {
+                padding: 1.5em;
+                left: 0;
+                top: 0;
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                display: block;
+                text-align: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-color: transparent;
+                background: #fff;
+                box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.26);
+
+            }
+
+            div.cardset div.card.front {
+                overflow: hidden;
+                z-index:1000;
+                -ms-transform: rotateX(0deg) rotateY(0deg);
+                -webkit-transform: rotateX(0deg) rotateY(0deg);
+                transform: rotateX(0deg) rotateY(0deg);
+                -webkit-transform-style: preserve-3d;
+                -ms-transform-style: preserve-3d;
+                transform-style: preserve-3d;
+                -webkit-backface-visibility: hidden;
+                backface-visibility: hidden;
+                -webkit-transition: all .3s ease-in-out;
+                transition: all .3s ease-in-out;
+            }
+
+            div.cardset.flip div.card.front {
+                z-index: 800;
+                -ms-transform: rotateY(180deg);
+                -webkit-transform: rotateY(180deg);
+                transform: rotateY(180deg);
+            }
+
+            div.cardset div.card.back {
+              background-image:
+              linear-gradient(180deg, white 3rem, #F0A4A4 calc(3rem), #F0A4A4 calc(3rem + 2px), transparent 1px),
+              repeating-linear-gradient(0deg, transparent, transparent 1.5rem, #DDD 1px, #DDD calc(1.5rem + 1px));
+              box-shadow: 1px 1px 3px rgba(0,0,0,.25);
+              background-size: auto 100%;
+              z-index: 800;
+              -ms-transform: rotateY(-180deg);
+              -webkit-transform: rotateY(-180deg);
+              transform: rotateY(-180deg);
+              -webkit-backface-visibility: hidden;
+              backface-visibility: hidden;
+              -webkit-transition: all .3s ease-in-out;
+              transition: all .3s ease-in-out;
+            }
+
+            div.cardset.flip div.card.back {
+                z-index: 1000;
+                -ms-transform: rotateX(0deg) rotateY(0deg);
+                -webkit-transform: rotateX(0deg) rotateY(0deg);
+                transform: rotateX(0deg) rotateY(0deg);
+            }
+
+                div.cardset div.card div.card-contents {
+                    position: relative;
+                    top: 50%;
+                    -webkit-transform: translateY(-50%);
+                    -ms-transform: translateY(-50%);
+                    transform: translateY(-50%);
+                }
+
+                    div.cardset div.card div.card-contents img {
+                        position: relative;
+                        max-height: 16rem;
+                    }
+
+                div.cardset div.card div.card-contents > *:first-child {
+                    margin-top: 0;
+                }
+  `]
+})
+export class Flashcards implements OnInit {
+
+  @HostListener('window:keydown', ['$event'])
+  keyboardInput(event: KeyboardEvent) {
+    if(event.code === 'BracketRight') {
+      this.changeCard('next');
+    }
+    if(event.code === 'BracketLeft') {
+      this.changeCard('prev');
+    }
+    if(event.code === 'KeyF') {
+      this.flipCard(this.flashcardsData.cardNumber);
+    }
+  }
+
+  @Input() data: any;
+
+  flashcardsData: any;
+
+  constructor( ) { }
+
+  ngOnInit() {
+    this.flashcardsData = this.data;
+    this.shuffleCards();
+  }
+
+  shuffle(sourceArray: any) {
+    for (var n = 0; n < sourceArray.length - 1; n++) {
+      var k = n + Math.floor(Math.random() * (sourceArray.length - n));
+      var temp = sourceArray[k];
+      sourceArray[k] = sourceArray[n];
+      sourceArray[n] = temp;
+    }
+  }
+
+  shuffleCards() {
+    if(this.flashcardsData.shuffle) {
+      this.shuffle(this.flashcardsData.cards);
+    }
+  }
+
+  changeCard(direction: string) {
+    this.flashcardsData.cards[this.flashcardsData.cardNumber].flipped = false;
+    if (direction === 'next' && this.flashcardsData.cardNumber < this.flashcardsData.cards.length - 1) {
+      this.flashcardsData.cardNumber++;
+    } else if (direction === 'prev' && this.flashcardsData.cardNumber > 0) {
+      this.flashcardsData.cardNumber--;
+    }
+  }
+
+  showCard(index: number) {
+    if (this.flashcardsData.cardNumber === index) {
+      return true;
+    }
+  }
+
+  flipCard(index: number) {
+    this.flashcardsData.cards[index].flipped = !this.flashcardsData.cards[index].flipped;
+  }
+
+  firstCard() {
+    if (this.flashcardsData.cardNumber === 0) {
+      return true;
+    }
+  }
+
+  lastCard() {
+    if (this.flashcardsData.cardNumber === this.flashcardsData.cards.length - 1) {
+      return true;
+    }
+  }
+
+}
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [Flashcards],
+  declarations: [Flashcards]
+})
+export class FlashcardsModule { }
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './components/layer-toggle/layer-toggle';
 export * from './components/points-of-interest/points-of-interest';
 export * from './components/quiz/quiz';
+export * from './components/flashcards/flashcards';
